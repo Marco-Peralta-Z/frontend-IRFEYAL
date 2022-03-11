decd318796afb17e9b9c38cdbff2c3ebd257e55b
@marcoperaltazeas <marcoperaltazeas@gmail.com>
2022-03-10 20:37:16 -0500

    footer irfeyal 1.2

--- pages/includes/footer.php
+++ pages/includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website 2021</div>
+            <div class="text-muted">Copyright &copy; IRFEYAL 2022</div>
             <div>
                 <a href="#">Privacy Policy</a>
                 &middot;
