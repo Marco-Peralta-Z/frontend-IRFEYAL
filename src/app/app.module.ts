4ca3bae4d2df9419bc9fd23405a26d7753746618
sebastian-16898 <sebastiansarango.16898@gmail.com>
2022-04-04 22:27:12 -0500

    Creacion del Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SplitterModule } from 'primeng/splitter';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { DropdownModule } from 'primeng/dropdown';
+import { DragDropModule } from 'primeng/dragdrop';
+import { DialogModule } from 'primeng/dialog';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { AccordionModule } from 'primeng/accordion';
+import { FileUploadModule } from 'primeng/fileupload';
+import { FormsModule } from '@angular/forms';
+import { CalendarModule } from 'primeng/calendar';
+import { TableModule } from 'primeng/table';
+import { InputMaskModule } from 'primeng/inputmask';
+import { ToolbarModule } from 'primeng/toolbar';
+import { KeyFilterModule } from 'primeng/keyfilter';
+import { BlockUIModule } from 'primeng/blockui';
+import { FocusTrapModule } from 'primeng/focustrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { DividerModule } from 'primeng/divider';
+import { TooltipModule } from 'primeng/tooltip';
+import { DatePipe, HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { AsignaturaService } from './Servicio/parametrizacion/asignatura.service';
+import {MegaMenuModule} from 'primeng/megamenu';
+import {MenubarModule} from 'primeng/menubar'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, KeyFilterModule,MegaMenuModule,
+    AppRoutingModule, ToastModule, BrowserAnimationsModule,
+    SplitterModule,MenubarModule,
+    ButtonModule, FocusTrapModule, BlockUIModule,
+    InputTextModule, OverlayPanelModule, DividerModule, TooltipModule,
+    DialogModule, PanelMenuModule
   ],
-  providers: [],
+  providers: [AsignaturaService, DatePipe, { provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
