1eadbc176fb1f3e9c1b347cb3bfec4ce89e4d515
JaviKo500 <javikogutierrez64@gmail.com>
2022-05-24 11:24:06 -0500

    creo componente kit y modulo

---
+++ src/app/pages/inventarios/modulo/modulo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modulo',
+  templateUrl: './modulo.component.html',
+  styleUrls: ['./modulo.component.css']
+})
+export class ModuloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
