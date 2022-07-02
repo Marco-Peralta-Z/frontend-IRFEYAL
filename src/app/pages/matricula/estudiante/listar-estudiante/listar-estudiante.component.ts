c0bb1e64c0f44cfc054504945aeb1c6e6b562d8b
Jonathan Yagual <vycentkro27@gmail.com>
2022-07-01 20:11:57 -0500

    update

--- src/app/pages/matricula/estudiante/listar-estudiante/listar-estudiante.component.ts
+++ src/app/pages/matricula/estudiante/listar-estudiante/listar-estudiante.component.ts
@@ -8,6 +8,8 @@ import { canton } from '../../../../Model/rolesTS/canton';
 import { parroquia } from '../../../../Model/rolesTS/parroquia';
 import { genero } from '../../../../Model/rolesTS/genero';
 import { DatePipe } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ValidatorsService } from '../../../../Servicio/moduloMatricula/Validators/validators.service';
 @Component({
   selector: 'app-listar-estudiante',
   templateUrl: './listar-estudiante.component.html',
@@ -27,24 +29,64 @@ export class ListarEstudianteComponent implements OnInit {
   productDialog?: boolean;
   fecha: string= '';
   fechaActual= new Date();
+  age:number=0;
   year?: number;
+  existe: boolean = true;
   month?:number;
   day?:number;
   mesFinal:any;
   diaFinal:any
+  camposRequeridos: boolean=true;
   rows: number=5;
   pipe = new DatePipe('en-EC');
   estudiante: Estudiante= new Estudiante();
-
+  validador?: boolean;
   selectEstudiante: Estudiante[]=[];
   esditable: boolean = true;
   submitted?: boolean;
-
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
   statuses?: any[];
 
+
+  editEstudiante:FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    apellido: [, [Validators.required, Validators.minLength(3)]],
+    cedula: [, [Validators.required]],
+    fecha: [[], [Validators.required, this.noPuedeSerNull]],
+    genero: [[], [Validators.required]],
+    provincia: [[], Validators.required],
+    canton: [[], [Validators.required]],
+    parroquia: [[], [Validators.required]],
+    calleFirts: [, [Validators.required]],
+    calleSecond: [, [Validators.required]],
+    telefono: [, [Validators.required]],
+    celular: [, [Validators.required]],
+    email: [, [Validators.required, Validators.pattern(this.emailPattern)]]
+  });
+
   constructor(private estudianteService: EstudianteService, 
-    private messageService: MessageService, 
+    private messageService: MessageService,
+    private fb: FormBuilder, 
+    private validatorService: ValidatorsService,
     private confirmationService: ConfirmationService) { }
+
+    noPuedeSerNull(control: FormControl) {
+      const fecha = control.value; 
+      const today: Date = new Date();
+      const birthDate: Date = new Date(fecha);
+      let edad:number = today.getFullYear() - birthDate.getFullYear();
+      const month: number = today.getMonth() - birthDate.getMonth();
+      if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+          edad--;
+      }
+      if (edad<18) {
+        return {
+          fecha: true
+        }
+      }
+      return null;
+    }
+
 
   ngOnInit() {
     this.estudianteService.getEstudiantes()
@@ -121,22 +163,45 @@ export class ListarEstudianteComponent implements OnInit {
   }
 
   saveEstudent() {
-      this.submitted = true;
-      this.productDialog = false;
+
+    if (this.editEstudiante.invalid || this.age<18) {
+      this.editEstudiante.markAllAsTouched();
+      this.messageService.add({ key: 'tc', severity: 'error', summary: 'Error en Formulario', detail: 'Campos erroneos o incompletos' });
+      // swal.fire('Error en Formulario', 'Campos erroneos o incompletos', 'error');
+      return;
+    } 
+      // this.submitted = true;
+      
 // modificar para editar al estudiante obteniendo su id 
-      if (this.estudiante.id_persona.cedula.trim()) {
+      if (this.existe) {
+        this.productDialog = false;
           if (this.estudiante.id_estudiante) {
-              console.log(this.estudiante.id_persona.fechaNacimiento);
               this.estudiantes[this.findIndexById(this.estudiante.id_estudiante)] = this.estudiante;              
               this.estudianteService.putEstudiante(this.estudiante)
               .subscribe(estudiante => (console.log(estudiante)));
-
-              
-              this.messageService.add({severity:'success', summary: 'Actualizado', detail: 'Información Actualizada', life: 3000});
+              this.messageService.add({key: 'tc',severity:'success', summary: 'Actualizado', detail: 'Información Actualizada', life: 3000});
           }
+      }  else {
+        this.messageService.add({ key: 'tc', severity: 'error', summary: 'Error de Datos', detail: `Cedula/Pasaporte ingresado incorrecto` });
+        
       }
   }
 
+  validaExistenciaEstudiante(): boolean {
+    this.validador=this.validatorService.validarCedu(this.estudiante.id_persona.cedula.trim());
+  if (this.validador) {
+    this.existe = true;
+  }else{
+      this.validador= this.validatorService.esAlfanumerico(this.estudiante.id_persona.cedula.trim());
+      if (this.validador) {
+        this.existe = true;
+      }else{
+        this.messageService.add({ key: 'tc', severity: 'error', summary: 'Error de Datos', detail: `Cedula/Pasaporte ingresado incorrecto` });
+         this.existe = false;
+      }
+  }
+  return this.existe;
+}
   findIndexById(id: number): number {
       let index = -1;
       for (let i = 0; i < this.estudiantes.length; i++) {
@@ -150,5 +215,24 @@ export class ListarEstudianteComponent implements OnInit {
   }
 mostrar(event:any){
 console.log(event);
+}
+
+CalculateAge() {
+  const today: Date = new Date();
+ const birthDate: Date = new Date(this.estudiante.id_persona.fechaNacimiento!);
+ this.age = today.getFullYear() - birthDate.getFullYear();
+ const month: number = today.getMonth() - birthDate.getMonth();
+ if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+     this.age--;
+ }
+ if (this.age<18) {
+   this.messageService.add({ key: 'tc', severity: 'warn', summary: 'Menor de Edad', detail: `Fecha ingresada incorrecta`});
+ }
+
+}
+ // validaciones de campos del formulario
+ campoValido(valor: string): boolean {
+  return this.editEstudiante.controls[valor].errors!
+    && this.editEstudiante.controls[valor].touched
 }
 }
