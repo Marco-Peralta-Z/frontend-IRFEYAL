f28466d40b8dc5e42b1f9144755db6906cf76d6e
DiegoBerrezueta <diego.berrezueta.est@tecazuay.edu.ec>
2022-08-01 22:16:15 -0500

    Commit
    Documentos Academicos

--- src/app/Servicio/documentacion_academica/planunidadServices/planunidad.service.ts
+++ src/app/Servicio/documentacion_academica/planunidadServices/planunidad.service.ts
@@ -53,8 +53,12 @@ export class PlanunidadService {
     return this.httpClient.get(this.API_SERVER + id_unidad + "/" + id_asignatura + "/" + id_curso + "/" + id_paralelo + "/" + id_periodo + "/" + id_modalidad);
   }
 
-  public createPDFplanunidad(planunidad: any): Observable<any> {
-    return this.httpClient.post(this.API_SERVER + "pdfPlanUnidad", planunidad);
+  public createPDFplanunidad(planunidad: any, coorPedagogico: string): Observable<any> {
+    return this.httpClient.post(this.API_SERVER + "pdfPlanUnidad/" + coorPedagogico, planunidad);
+  }
+
+  public getNomUsuariosByRolCoorPedagogico(): Observable<any> {
+    return this.httpClient.get(this.API_SERVER + "coorpedagogico");
   }
 
   //Consumido de parametrizacion
@@ -69,6 +73,10 @@ export class PlanunidadService {
 
   public getAllAsignaturasByMalla(idMalla: any): Observable<any> {
     return this.httpClient.get(this.API_SERVER + "asignaturas/malla/" + idMalla);
+  }
+
+  public getAllAreaByAsignatura(idAsignatura: any): Observable<any> {
+    return this.httpClient.get(this.API_SERVER + "area/asignatura/" + idAsignatura);
   }
 
   public getAllCurso(): Observable<any> {
