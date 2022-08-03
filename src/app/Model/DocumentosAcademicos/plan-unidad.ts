bc50c19f85cdb7a3648ba85f004f495b33b20e27
DiegoBerrezueta <diego.berrezueta.est@tecazuay.edu.ec>
2022-08-03 17:44:39 -0500

    Commit
    Documentos Academicos

--- src/app/Model/DocumentosAcademicos/plan-unidad.ts
+++ src/app/Model/DocumentosAcademicos/plan-unidad.ts
@@ -9,14 +9,27 @@ import { Periodo } from "../Parametrizacion/Periodo";
 export class PlanUnidad {
     id_plan_unidad: number = 0;
     titulo_unidad: string = "";
-    objetivos: string = "";
-    contenidos: string = "";
-    criterios_evaluacion: string = "";
-    destrezas: string = "";
+    fecha_creacion?: Date;
     fecha_inicio?: Date;
     fecha_fin?: Date;
+    num_periodos: number = 0;
+    objetivos: string = "";
+    criterios_evaluacion: string = "";
+    destrezas: string = "";
+    act_experiencia: string = "";
+    act_reflexion: string = "";
+    act_conceptualizacion: string = "";
+    act_aplicacion: string = "";
+    recursos: string = "";
+    indicadores: string = "";
+    tecnicas: string = "";
+    adaptaciones_curriculares: string = "";
+    adap_necesidad_educativa: string = "";
+    especificacion_nesesidad: string = "";
     estado: string = "";
-    obsevaciones: string = "";
+    observaciones: string = "";
+    coor_academico: string = "";
+    fecha_revision?: Date;
     unidad: Unidad = new Unidad();
     empleado: empleado = new empleado();
     asignatura: Asignatura = new Asignatura();
@@ -25,4 +38,5 @@ export class PlanUnidad {
     modalidad: Modalidad = new Modalidad();
     periodo: Periodo = new Periodo();
 }
+
 
